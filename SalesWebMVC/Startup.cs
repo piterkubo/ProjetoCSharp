60d9cdfe4179743ff8eddd0a9ebfc7579bd6ed32
piter kubo <piter kubo@cambio.com>
2024-01-04 10:11:00 -0300

    Criação do banco Mysql e Migration

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -33,12 +33,16 @@ namespace SalesWebMVC
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            
+            //alterar 
             services.AddDbContext<SalesWebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMVCContext"),builder => 
+                        builder.MigrationsAssembly("SalesWebMVC")));
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
