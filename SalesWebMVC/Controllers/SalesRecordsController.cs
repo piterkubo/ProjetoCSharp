0abd9ca3c9ce712e2182e0b2b4c428372fb99648
piter kubo <piter kubo@cambio.com>
2024-01-08 17:32:34 -0300

    Preparing sales search navigation views

---
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        // criando a busca simples
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+
+        // criando a pesquisa de grupo
+
+        public IActionResult GroupSearch()
+        {
+            return View();
+        }
+    }
+}
